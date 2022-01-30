ff8f66e848abfb899b1a24917afe873f3a2970d7
Sergey Yurin <sergey.yurin@yahoo.com>
2022-01-30 14:20:48 -0600

    home page update

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className='header-wraper'>
+      <div className='main-info'>
+        <h1>Health and Auto Insurance</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
